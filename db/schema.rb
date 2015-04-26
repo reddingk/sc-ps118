91cda62d441fb96f4d5ac792342dcfd1972e4ba2
Kris Redding <Kris.Redding3@gmail.com>
2015-04-26 17:53:13 +0000

    fixed missing email in users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424183451) do
+ActiveRecord::Schema.define(version: 20150426174943) do
 
   create_table "devices", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150424183451) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "email"
   end
 
 end
