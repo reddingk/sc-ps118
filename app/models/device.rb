eafa5592ffa246a0eb1b0e1e025c3127190acabb
Kris Redding <Kris.Redding3@gmail.com>
2015-04-26 17:31:40 +0000

    -3

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,5 @@
 class Device < ActiveRecord::Base
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :userid, presence: true
+    
 end
--- app/models/sfile.rb
+++ app/models/sfile.rb
@@ -1,2 +1,4 @@
 class Sfile < ActiveRecord::Base
+    mount_uploader :syncfile, SyncfileUploader
+    validates :name, :userid, :devid, presence: true
 end
